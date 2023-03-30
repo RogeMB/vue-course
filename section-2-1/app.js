cbe637b927b658eabc5a6d1513489ca720241971
RogeMB <rogelio.mb@hotmail.com>
2023-03-30 18:00:14 +0200

    data-binding, events

---
+++ section-2-1/app.js
@@ -0,0 +1,53 @@
+const vm = Vue.createApp({
+        data() {
+        return {
+            firstName: 'Roge',
+            lastName: 'Mohi',
+            url: 'https://triana.salesianos.edu/',
+            raw_url: '<a href="https://google.com" target= "_blank">Google</a>' ,
+            age: 20,
+            userName: 'Triana',
+        }
+    },
+    methods: {
+        fullName() {
+            return  `${this.firstName.toUpperCase()} ${this.lastName}`
+        },
+
+        increment() {
+            this.age++;
+        },
+
+        updateLastName(event) {
+            this.lastName = event.target.value
+        },
+
+        updateUserName(str, event) {
+            event.preventDefault()
+            console.log(str)
+            this.userName = event.target.value
+        }
+
+
+    }
+}).mount('#app');
+
+// Igual que escribir app.mount('#app');
+
+setTimeout( () => {
+    vm.firstName = 'Javier'; // * that is with proxy. Without proxy -> vm.$data.firstName
+}, 2000);
+
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             firstName: 'Roge',
+//             lastName: 'Mohi',
+//         }
+//     }
+// }).mount('#app2');
+
+// * podemos crear tantas apps como queramos.
+
+
